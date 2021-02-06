6bd9e74217224e2f4019a60a23aae3ec4c6bcb78
unknown <taylorrec1991@gmail.com>
2021-02-06 03:35:13 -0500

    added new files

--- module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountSqlDAO.java
+++ module-2/15_Capstone/pair-exercise/java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountSqlDAO.java
@@ -1,35 +1,36 @@
-//package com.techelevator.tenmo.dao;
-//
-//import java.math.BigDecimal;
-//
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.support.rowset.SqlRowSet;
-//
-//import com.techelevator.tenmo.model.Account;
-//
-//public class AccountSqlDAO implements AccountDAO
-//{
-//	private JdbcTemplate jdbcTemplate;
-//	
-//	public Account getBalance(int accountId)
-//	{
-//		Account balance = null;
-//		
-//		// build the query
-//		String sql = "SELECT *" + 
-//					 "FROM accounts" + 
-//					 "WHERE account_id = ?;";
-//		
-//		SqlRowSet row = jdbcTemplate.queryForRowSet(sql, accountId);
-//		
-//		if (row.next())
-//		{
-//			balance = mapRowToAccount(row);
-//		}
-//		
-//		return balance;
-//		
-//	}
+package com.techelevator.tenmo.dao;
+import java.math.BigDecimal;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.tenmo.model.Account;
+
+
+
+
+public class AccountSqlDAO implements AccountDAO{
+
+private static final Object accountId = null;
+private JdbcTemplate jdbcTemplate;
+
+	public void JDBCAccountSqlDAO(DataSource datasource) {
+		this.jdbcTemplate = new JdbcTemplate(datasource);
+	}
+	
+	public BigDecimal getBalance(int accountId)
+	{	
+	// build the query
+	String sqlFindAccountsById= "SELECT *" + 
+					 "FROM accounts" + 
+					 "WHERE account_id = ?;";
+	return null;
+	
+		       
+	}
+
 //
 //	@Override
 //	public BigDecimal addToBalance(BigDecimal amountToAdd, int id)
@@ -72,3 +73,29 @@
 //	
 //	
 //}
+
+
+	@Override
+	public BigDecimal addToBalance(BigDecimal amountToAdd, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigDecimal subtractFromBalance(BigDecimal amountToSubtract, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account findUserById(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Account findAccountById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
