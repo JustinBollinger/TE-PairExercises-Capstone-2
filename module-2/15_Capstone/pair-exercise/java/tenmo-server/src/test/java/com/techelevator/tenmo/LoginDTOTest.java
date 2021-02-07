b4433d0a525b61222ae2caca83c3a5be13d0c879
Susan Cole <scole19@mac.com>
2021-02-07 15:57:43 -0500

    tests

--- module-2/15_Capstone/pair-exercise/java/tenmo-server/src/test/java/com/techelevator/tenmo/LoginDTOTest.java
+++ module-2/15_Capstone/pair-exercise/java/tenmo-server/src/test/java/com/techelevator/tenmo/LoginDTOTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import com.techelevator.tenmo.model.LoginDTO;
 
-public class LoginDTOTest {
+public class LoginDTOTest
+{
 	
 	
 	LoginDTO login = new LoginDTO();
